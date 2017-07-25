7e8c0809da8d6d230ab8300dba034c661c0c0353
dgrossman <dgrossman@iqt.org>
2017-07-25 14:07:55 -0700

    make output json, remove datetime object putin time.time() instead

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 import gpustat
+import json
+import time
+import pprint
+
 
 app = Flask(__name__)
 
@@ -7,7 +11,9 @@ app = Flask(__name__)
 @app.route('/')
 def get_status():
     data = gpustat.print_gpustat(**{'show_cmd': True, 'gpuname_width': 16, 'show_user': True, 'show_pid': True, 'json': True, 'no_color': False, 'ret_json': True} )    
-    return('{0}'.format(data))
+    data['query_time']= time.time()
+    
+    return('{0}'.format(json.dumps(data,indent=4)))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
