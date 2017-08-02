4c2aba41e1a43c9b773a1eafed6d1b23441e2e62
dgrossman <dgrossman@iqt.org>
2017-08-02 12:49:45 -0700

    adding support for ajax

--- flask_app.py
+++ flask_app.py
@@ -20,6 +20,19 @@ def get_status():
     
     return('{0}'.format(json.dumps(data,indent=4)))
 
+@app.route('/gpu')
+def get_status2():
+    data = gpustat.print_gpustat(**{'show_cmd': False, 'gpuname_width': 16, 'show_user': False, 'show_pid': False, 'json': True, 'no_color': False, 'ret_json': True})
+    data['query_time']= time.time()
+    
+    retval= '{0}'.format(json.dumps(data,indent=4))
+    return retval,201,{'Access-Control-Allow-Origin': '*'}
+
+def options (self):
+    return {'Allow' : 'PUT' }, 200, \
+    { 'Access-Control-Allow-Origin': '*', \
+      'Access-Control-Allow-Methods' : 'PUT,GET' }
+
 if __name__ == "__main__":
     port = 5000
     if len(sys.argv) > 1:
