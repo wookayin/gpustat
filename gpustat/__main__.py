226595055f4dcf8e8918058f08fe868e9b682421
Tzu-Wei Huang <huang.dexter@gmail.com>
2018-01-29 14:13:24 +0800

    Add option to loop forever (nvidia-smi -l)

--- gpustat/__main__.py
+++ gpustat/__main__.py
@@ -12,20 +12,26 @@ def print_gpustat(json=False, debug=False, **args):
     '''
     Display the GPU query results into standard output.
     '''
-    try:
-        gpu_stats = GPUStatCollection.new_query()
-    except Exception as e:
-        sys.stderr.write('Error on querying NVIDIA devices. Use --debug flag for details\n')
-        if debug:
-            import traceback
-            traceback.print_exc(file=sys.stderr)
-        sys.exit(1)
-
-    if json:
-        gpu_stats.print_json(sys.stdout)
-    else:
-        gpu_stats.print_formatted(sys.stdout, **args)
-
+    isloop = args.pop('loop')
+    while True:
+        try:
+            gpu_stats = GPUStatCollection.new_query()
+        except Exception as e:
+            sys.stderr.write('Error on querying NVIDIA devices. Use --debug flag for details\n')
+            if debug:
+                import traceback
+                traceback.print_exc(file=sys.stderr)
+            sys.exit(1)
+        except KeyboardInterrupt:
+            gpu_stats.print_formatted(sys.stdout, **args)
+            sys.exit(1)
+        if json:
+            gpu_stats.print_json(sys.stdout)
+        else:
+            gpu_stats.print_formatted(sys.stdout, **args)
+        if isloop:
+            break
+        print('\x1b[A'*(len(gpu_stats)+2))
 
 def main(*argv):
     if not argv:
@@ -45,6 +51,8 @@ def main(*argv):
     parser_color.add_argument('--no-color', action='store_true',
                               help='Suppress colored output')
 
+    parser.add_argument('-l', '--loop', action='store_false',
+                        help='Loop until Ctrl+C')
     parser.add_argument('-c', '--show-cmd', action='store_true',
                         help='Display cmd name of running process')
     parser.add_argument('-u', '--show-user', action='store_true',
