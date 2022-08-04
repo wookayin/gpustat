4400d6452646f388d7d543613ab905d49ef87ba3
Jongwook Choi <wookayin@gmail.com>
2022-08-04 13:54:18 -0400

    Introduce a temporary way to bypass pynvml valdiation
    gpustat 1.0.0+ requires pynvml to be an official one (nvidia-ml-py)
    rather than an obsolete third-party package nvidia-ml-py3 (see #107),
    but this may cause some inconvenient conflict with other third-party
    packages that depend on the legacy nvidia-ml-py3.
    
    As a temporary workaround, we introduce an environment variable
    `ALLOW_LEGACY_PYNVML` whose use will result in bypassing pynvml
    validation. With this flag turned on, gpustat may work with the legacy
    pynvml library, but with a possibility that it may produce wrong
    results on running process information.
    
    e.g., ALLOW_LEGACY_PYNVML=1 gpustat

--- gpustat/nvml.py
+++ gpustat/nvml.py
@@ -1,9 +1,18 @@
 """Imports pynvml with sanity checks and custom patches."""
 
 import textwrap
+import os
 
 
 pynvml = None
+
+# If this environment variable is set, we will bypass pynvml version validation
+# so that legacy pynvml (nvidia-ml-py3) can be used. This would be useful
+# in a case where there are conflicts on pynvml dependencies.
+# However, beware that pynvml might produce wrong results (see #107).
+ALLOW_LEGACY_PYNVML = os.getenv("ALLOW_LEGACY_PYNVML", "")
+ALLOW_LEGACY_PYNVML = ALLOW_LEGACY_PYNVML.lower() not in ('false', '0', '')
+
 
 try:
     # Check pynvml version: we require 11.450.129 or newer.
@@ -14,7 +23,7 @@ try:
         hasattr(pynvml, 'NVML_BRAND_NVIDIA_RTX') or
         # Requires nvidia-ml-py >= 11.450.129, < 11.510.69
         hasattr(pynvml, 'nvmlDeviceGetComputeRunningProcesses_v2')
-    ):
+    ) and not ALLOW_LEGACY_PYNVML:
         raise RuntimeError("pynvml library is outdated.")
 except (ImportError, SyntaxError, RuntimeError) as e:
     raise ImportError(textwrap.dedent(
@@ -30,7 +39,7 @@ except (ImportError, SyntaxError, RuntimeError) as e:
 
         $ pip install --force-reinstall gpustat
 
-        if it still does not fix the problem, manually fix nvidia-ml-py installation:
+        If it still does not fix the problem, please manually fix nvidia-ml-py installation:
 
         $ pip uninstall nvidia-ml-py3
         $ pip install --force-reinstall 'nvidia-ml-py<=11.495.46'
