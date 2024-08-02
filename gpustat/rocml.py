bf1a00a4fc426e6c2b01b97519a2ce83861781da
brnelson <bryant.nelson@amd.com>
2024-08-02 20:23:34 +0000

    Format new file.

--- gpustat/rocml.py
+++ gpustat/rocml.py
@@ -11,25 +11,24 @@ import warnings
 
 from collections import namedtuple
 
-
-
-
-
 try:
     # Check for amdsmi.
     from amdsmi import *
 except (ImportError, SyntaxError, RuntimeError) as e:
-    _amdsmi = sys.modules.get('amdsmi', None)
+    _amdsmi = sys.modules.get("amdsmi", None)
 
-    raise ImportError(textwrap.dedent(
-        """\
+    raise ImportError(
+        textwrap.dedent(
+            """\
         amdsmi is missing or an outdated version is installed.
 
-        The root cause: """ + str(e) +
-        """
+        The root cause: """
+            + str(e)
+            + """
 
-        Your pynvml installation: """ + repr(_amdsmi) +
-        """
+        Your pynvml installation: """
+            + repr(_amdsmi)
+            + """
 
         -----------------------------------------------------------
         (Suggested Fix) Please install amdsmi.
@@ -40,19 +39,24 @@ except (ImportError, SyntaxError, RuntimeError) as e:
         cd /opt/rocm/share/amd_smi
         python3 -m pip install --upgrade pip
         python3 -m pip install --user .
-        """)) from e
+        """
+        )
+    ) from e
 
 NVML_TEMPERATURE_GPU = 1
+
 
 class NVMLError(Exception):
     def __init__(self, message="ROCM Error"):
         self.message = message
         super().__init__(self.message)
 
+
 class NVMLError_Unknown(Exception):
     def __init__(self, message="An unknown ROCM Error has occurred"):
         self.message = message
         super().__init__(self.message)
+
 
 class NVMLError_GpuIsLost(Exception):
     def __init__(self, message="ROCM Device is lost."):
@@ -64,6 +68,7 @@ _stdout_dup = os.dup(1)
 _stderr_dup = os.dup(2)
 _silent_pipe = os.open(os.devnull, os.O_WRONLY)
 
+
 def silent_run(to_call, *args, **kwargs):
     os.dup2(_silent_pipe, 1)
     os.dup2(_silent_pipe, 2)
@@ -72,11 +77,14 @@ def silent_run(to_call, *args, **kwargs):
     os.dup2(_stderr_dup, 2)
     return retval
 
+
 def nvmlDeviceGetCount():
     return len(amdsmi_get_processor_handles())
 
+
 def nvmlDeviceGetHandleByIndex(dev):
     return amdsmi_get_processor_handles()[dev]
+
 
 def nvmlDeviceGetIndex(dev):
     for i, handle in enumerate(amdsmi_get_processor_handles()):
@@ -84,17 +92,22 @@ def nvmlDeviceGetIndex(dev):
             return i
     return -1
 
+
 def nvmlDeviceGetName(dev):
     return amdsmi_get_gpu_board_info(dev)["product_name"]
+
 
 def nvmlDeviceGetUUID(dev):
     return amdsmi_get_gpu_device_uuid(dev)
 
+
 def nvmlDeviceGetTemperature(dev, loc=NVML_TEMPERATURE_GPU):
     return amdsmi_get_temp_metric(dev, AmdSmiTemperatureType.HOTSPOT, AmdSmiTemperatureMetric.CURRENT)
 
+
 def nvmlSystemGetDriverVersion():
     return amdsmi_get_gpu_driver_info(amdsmi_get_processor_handles()[0])["driver_version"]
+
 
 def check_driver_nvml_version(driver_version_str: str):
     """Show warnings when an incompatible driver is used."""
@@ -105,11 +118,11 @@ def check_driver_nvml_version(driver_version_str: str):
         except (ValueError, TypeError):
             return 0
 
-    driver_version = tuple(safeint(v) for v in
-                           driver_version_str.strip().split("."))
+    driver_version = tuple(safeint(v) for v in driver_version_str.strip().split("."))
 
     if driver_version < (6, 7, 8):
         warnings.warn(f"This version of ROCM Driver {driver_version_str} is untested, ")
+
 
 def nvmlDeviceGetFanSpeed(dev):
     try:
@@ -117,30 +130,39 @@ def nvmlDeviceGetFanSpeed(dev):
     except Exception:
         return None
 
-MemoryInfo = namedtuple('MemoryInfo', ['total', 'used'])
+
+MemoryInfo = namedtuple("MemoryInfo", ["total", "used"])
+
 
 def nvmlDeviceGetMemoryInfo(dev):
-    return MemoryInfo(total=amdsmi_get_gpu_memory_total(dev, AmdSmiMemoryType.VRAM),
-                      used=amdsmi_get_gpu_memory_usage(dev, AmdSmiMemoryType.VRAM))
+    return MemoryInfo(total=amdsmi_get_gpu_memory_total(dev, AmdSmiMemoryType.VRAM), used=amdsmi_get_gpu_memory_usage(dev, AmdSmiMemoryType.VRAM))
 
-UtilizationRates = namedtuple('UtilizationRates', ['gpu'])
+
+UtilizationRates = namedtuple("UtilizationRates", ["gpu"])
+
 
 def nvmlDeviceGetUtilizationRates(dev):
     return UtilizationRates(gpu=amdsmi_get_gpu_activity(dev)["gfx_activity"])
 
+
 def nvmlDeviceGetEncoderUtilization(dev):
     return None
+
 
 def nvmlDeviceGetDecoderUtilization(dev):
     return None
 
+
 def nvmlDeviceGetPowerUsage(dev):
     return amdsmi_get_power_info(dev)["current_socket_power"] * 1000
+
 
 def nvmlDeviceGetEnforcedPowerLimit(dev):
     return amdsmi_get_power_info(dev)["power_limit"] * 1000
 
-ComputeProcess = namedtuple('ComputeProcess', ['pid', 'usedGpuMemory'])
+
+ComputeProcess = namedtuple("ComputeProcess", ["pid", "usedGpuMemory"])
+
 
 def nvmlDeviceGetComputeRunningProcesses(dev):
     try:
@@ -149,8 +171,10 @@ def nvmlDeviceGetComputeRunningProcesses(dev):
     except Exception:
         return []
 
+
 def nvmlDeviceGetGraphicsRunningProcesses(dev):
     return None
+
 
 def nvmlDeviceGetClockInfo(dev, clk_type=AmdSmiClkType.SYS):
     result = amdsmi_get_clock_info(dev, clk_type)
@@ -159,9 +183,11 @@ def nvmlDeviceGetClockInfo(dev, clk_type=AmdSmiClkType.SYS):
     else:
         return result["cur_clk"]
 
+
 def nvmlDeviceGetMaxClockInfo(dev, clk_type=AmdSmiClkType.SYS):
     result = amdsmi_get_clock_info(dev, clk_type)
     return result["max_clk"]
+
 
 # Upon importing this module, let amdsmi be initialized and remain active
 # throughout the lifespan of the python process (until gpustat exists).
@@ -173,6 +199,7 @@ try:
 
     def _shutdown():
         amdsmi_shut_down()
+
     atexit.register(_shutdown)
 
 except Exception as exc:
@@ -183,6 +210,3 @@ except Exception as exc:
 def ensure_initialized():
     if not _initialized:
         raise _init_error  # type: ignore
-
-
-
